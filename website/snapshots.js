5d9429c6635d51341f176c95881690665c6de832
Rohit Gohri <code@rohit.page>
2022-03-17 18:34:20 +0530

    add dark mode snapshots

--- website/snapshots.js
+++ website/snapshots.js
@@ -9,10 +9,28 @@ const urls = [
 
 module.exports = () => {
   const baseUrl = process.env.TARGET_URL || 'http://localhost:3000';
-  return urls.map(({ name, path }) => {
-    return {
-      name,
-      url: `${baseUrl}${path}`,
-    };
-  });
+  return [
+    ...urls.map(({ name, path }) => {
+      return {
+        name,
+        url: `${baseUrl}${path}`,
+      };
+    }),
+    ...urls.map(({ name, path }) => {
+      return {
+        name: `${name} (Dark Mode)`,
+        url: `${baseUrl}${path}`,
+        execute: {
+          afterNavigation() {
+            // toggle dark-mode
+            // @ts-ignore
+            Array.from(document.querySelector('.navbar__items--right').children)
+              .pop()
+              // @ts-ignore
+              .children[0].click();
+          },
+        },
+      };
+    }),
+  ];
 };
